3a04135ac24553f43096da1366de4a6d0aee16a2
Sinan Hacı <sinan.hacia@hotmail.com>
2021-02-16 17:10:25 +0300

    6 Şubat

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -2,13 +2,12 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal:IEntityRepository<Category>
+    public interface IOrderDal :IEntityRepository<Order>
     {
-        
+
     }
 }
